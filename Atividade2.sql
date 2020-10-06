4903abac4ff5170b7063934a9e16c8d1db36a8bc
gu1rocha <59288859+gu1rocha@users.noreply.github.com>
2020-10-06 09:28:41 -0300

    Update Atividade2.sql
    Especificação dos itens pedidos para criação do banco

--- Atividade2.sql
+++ Atividade2.sql
@@ -1,4 +1,6 @@
-/* ITEM A*/
+/* ITEM A: 
+Crie o banco de dados, chamado “carros” e as tabelas marca e modelo. Lembrese de criar as chaves primárias como geradas automaticamente e a chave estrangeira entre a tabela Modelo e a tabela Marca.
+*/
 CREATE DATABASE carros;
 USE carros;
 CREATE TABLE marca(
@@ -16,7 +18,9 @@ CREATE TABLE modelo(
     CONSTRAINT FK_marca FOREIGN KEY (codigo_marca) REFERENCES marca (codigo)
 );
 
-/* ITEM B*/
+/* ITEM B:
+Crie os comandos para realizar a inserção das 4 marcas e dos 4 modelosapresentados na tabela;
+*/
 INSERT INTO marca (nome,pais) 
     VALUES ('Volkswagen','Alemanha'),('Ford','EUA'),('Fiat','Itália'),('GM','EUA');
     
@@ -26,15 +30,21 @@ INSERT INTO modelo (nome,categoria,cor,codigo_marca)
     ('Ecosport','SUV','azul','2'),
     ('Fox','Família','preto','1');
 
-/* ITEM C*/
+/* ITEM C:
+Crie um comando para alterar a cor do carro Fox para cinza, usando sua chave primária.
+*/
 UPDATE modelo SET cor = 'cinza' WHERE codigo = 4;
 
-/* ITEM D*/
+/* ITEM D:
+Crie uma consulta que seja capaz de exibir nome da marca, país e quantidade de carros daquela marca que a empresa possui.
+*/
 SELECT ma.nome, ma.pais, COUNT(mo.codigo_marca) AS qtd_carros 
 FROM marca AS ma
 LEFT JOIN modelo AS mo
 ON ma.codigo = mo.codigo_marca
 GROUP BY ma.nome;
 
-/* ITEM E*/
+/* ITEM E:
+Crie um comando para excluir a marca GM, usando sua chave primária.
+*/
 DELETE FROM marca WHERE codigo = 4;
